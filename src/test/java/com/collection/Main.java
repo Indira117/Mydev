8afa82d4235df3c755741f5603d6917733febda2
Indira117 <indirazhana@gmail.com>
2021-03-10 18:19:55 -0500

    1111

--- src/test/java/com/collection/Main.java
+++ src/test/java/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 11; i++) {
             System.out.println("hello");
         }
     }
